ddc70767771aac385bf1c03c9365671a18f76d14
MICKEY♡ <88245294+D15H4NT0P@users.noreply.github.com>
2021-08-28 13:02:51 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it
-net stop VAMP-X
-net start VAMP-X
+net stop VAMPxROBOT
+net start VAMPxROBOT
